998ad43d302cac25e7ae9ba5c31a826c7d84cb10
Aleksandr Shabanov <vonabahs.a@gmail.com>
2021-03-25 15:51:33 +0300

    Added tasks of the first unit

---
+++ src/main/java/unit1/Calculator.java
@@ -0,0 +1,34 @@
+package unit1;
+
+public class Calculator {
+    public static void main(String args[]) {
+        Calculator calc = new Calculator();
+
+        System.out.println(calc.getSum(1, 4));
+        System.out.println(calc.getMinus(2,2));
+        System.out.println(calc.getDivision(20,2));
+        System.out.println(calc.getMultiplication(5,5));
+    }
+
+    public int getSum(int param1, int param2) {
+        return param1 + param2;
+    }
+
+    public int getMinus(int param1, int param2){
+        return param1 - param2;
+    }
+
+    public int getDivision(int param1, int param2){
+        try {
+            return param1 / param2;
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Ошибка! Деление на ноль.");
+        }
+        return param1 / param2;
+    }
+
+    public int getMultiplication(int param1, int param2){
+        return param1 * param2;
+    }
+}
---
+++ src/main/java/unit1/StringUtilsTrial.java
@@ -0,0 +1,13 @@
+package unit1;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class StringUtilsTrial {
+
+    public static void main(String[] args) {
+
+        // Reverse a String
+        System.out.println("9) Reverse >>> " + StringUtils.reverse("REVERSE"));
+
+    }
+}
