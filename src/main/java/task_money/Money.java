6d19088332a67f4c7505e5099d6fb2184edf3cda
Aleksandr Shabanov <vonabahs.a@gmail.com>
2021-04-19 16:40:06 +0300

    Add task_money

--- src/main/java/task_money/Money.java
+++ src/main/java/task_money/Money.java
@@ -7,11 +7,86 @@ package task_money;
  * умножение на дробное число и операции сравнения. В функции main проверить эти методы.
  */
 public class Money {
+
     private long sum;
     private String moneyName;
 
     Money(long sum) {
         this.sum = sum;
+    }
+
+    Money() {
+    }
+
+    public long getSum() {
+        return sum;
+    }
+
+    public void setSum(long sum) {
+        this.sum = sum;
+    }
+
+    public String getMoneyName() {
+        return moneyName;
+    }
+
+    public void setMoneyName(String moneyName) {
+        this.moneyName = moneyName;
+    }
+
+    @Override
+    public String toString() {
+        return "Результат: " + sum;
+    }
+
+    public Money addition(Money money, long l) {
+        Money newMoney = new Money();
+        newMoney.setSum(money.getSum() + l);
+
+        return newMoney;
+    }
+
+    public Money subtraction(Money money, long l) {
+        Money newMoney = new Money();
+        newMoney.setSum(money.getSum() - l);
+
+        return newMoney;
+    }
+
+    public Money division(Money money, long l) {
+        Money newMoney = new Money();
+        newMoney.setSum(money.getSum() / l);
+
+        return newMoney;
+    }
+
+    public Money fractionalDivision(Money money, double d) {
+        Money newMoney = new Money();
+        newMoney.setSum((long) (money.getSum() / d));
+
+        return newMoney;
+    }
+
+    public Money multiplication(Money money, double d) {
+        Money newMoney = new Money();
+        if (money.getSum() == 0 || d == 0) {
+            throw new ArithmeticException();
+        } else {
+            newMoney.setSum((long) (money.getSum() * d));
+        }
+
+        return newMoney;
+    }
+
+    public Money comparison(Money money, long l) {
+        Money newMoney = new Money();
+
+        if (money.getSum() > l) {
+            newMoney.setSum(money.getSum());
+        } else {
+            newMoney.setSum(l);
+        }
+        return newMoney;
     }
 
 }
--- src/main/java/task_money/MoneyTest.java
+++ src/main/java/task_money/MoneyTest.java
@@ -2,6 +2,14 @@ package task_money;
 
 public class MoneyTest {
     public static void main(String[] args) {
+        Money rub = new Money(100);
+        rub.setMoneyName("rub");
 
+        System.out.println(rub.addition(rub,50) + " " + rub.getMoneyName());
+        System.out.println(rub.subtraction(rub, 80) + " " + rub.getMoneyName());
+        System.out.println(rub.division(rub, 5) + " " + rub.getMoneyName());
+        System.out.println(rub.fractionalDivision(rub, 5) + " " + rub.getMoneyName());
+        System.out.println(rub.multiplication(rub, 6789) + " " + rub.getMoneyName());
+        System.out.println(rub.comparison(rub, 50) + " " + rub.getMoneyName());
     }
 }
