1a26c5a8b6a6ae0c4ef9a6127a38abf875e0eb10
Aleksandr Shabanov <vonabahs.a@gmail.com>
2021-04-26 19:47:55 +0300

    Fix task_animal

--- src/main/java/task_animal/test/AnimalTest.java
+++ src/main/java/task_animal/test/AnimalTest.java
@@ -1,18 +1,15 @@
 package task_animal.test;
 
-import task_animal.models.Cat;
-import task_animal.models.Dog;
-import task_animal.models.Horse;
-import task_animal.models.Vet;
+import task_animal.models.*;
 
 /**
  * 4 задачка
  * ===========
- *
+ * <p>
  * Создать класс Animal и расширяющие его классы Dog, Cat, Horse.
  * Класс Animal содержит переменные food, location и методы makeNoise, eat, sleep.
  * Метод makeNoise, например, может выводить на консоль "Такое-то животное мурчит".
- *
+ * <p>
  * Dog, Cat, Horse переопределяют методы makeNoise, eat. Добавьте переменные в классы Dog, Cat, Horse,
  * характеризующие только этих животных. Создайте класс Ветеринар, в котором определите метод void treatAnimal(Animal animal).
  * Пусть этот метод распечатывает food и location пришедшего на прием животного. В методе main создайте массив типа Animal,
@@ -20,13 +17,19 @@ import task_animal.models.Vet;
  */
 public class AnimalTest {
     public static void main(String[] args) {
-        Horse horse = new Horse("сено", "прерии",  "Черная и длинная", 4);
+        Horse horse = new Horse("сено", "прерии", "Черная и длинная", 4);
         Dog dog = new Dog("косточки", "домашнее животное", "Длинный хвост", true);
         Cat cat = new Cat("сухой корм", "домашнее животное", true, 7);
 
-        Vet vet = new Vet();
-        vet.treatAnimal(horse);
-        vet.treatAnimal(dog);
-        vet.treatAnimal(cat);
+        Animal animals[] = new Animal[3];
+        animals[0] = horse;
+        animals[1] = dog;
+        animals[2] = cat;
+
+        for (int i = 0; i < animals.length; i++) {
+            Vet vet = new Vet();
+            vet.treatAnimal(animals[i]);
+        }
+
     }
 }
