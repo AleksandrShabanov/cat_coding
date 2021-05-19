db99e6633588aea2f71598380535960d5915e302
Aleksandr Shabanov <vonabahs.a@gmail.com>
2021-05-19 16:29:29 +0300

    Add task_reflection

---
+++ src/main/java/task_reflection/Reflection.java
@@ -0,0 +1,20 @@
+package task_reflection;
+
+public class Reflection {
+    private String name = "My name is";
+
+    private void printInfo() {
+        System.out.println("PrintInfo method");
+    }
+
+    public void openPrintInfo() {
+        System.out.println("Open printInfo method");
+    }
+
+    @Override
+    public String toString() {
+        return "Reflection{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/task_reflection/ReflectionTest.java
@@ -0,0 +1,37 @@
+package src.main.java.task_reflection;
+
+import task_reflection.Reflection;
+
+import java.lang.reflect.Field;
+
+/**
+ * 1) напиши программу, в которой попробуй сделать так чтобы ты смогу
+ * к строке добавить символы(модифицировать строку) и при этом чтобы hashcode данной строки не изменился,
+ * и изменения остались (адрес памяти не поменялся) - используй для этого рефлексию
+ * <p>
+ * String str = "test";
+ * int hashBeforeChanges = str.hash();
+ * ...//модификация строки
+ * int hashAfterChanges = str.hash();
+ * sout(hashBeforeChanges == hashAfterChanges);//должно вернуть true
+ */
+
+public class ReflectionTest {
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        Object obj = new Reflection();
+        Class aClass = obj.getClass();
+
+        Field nameField = aClass.getDeclaredField("name");
+        nameField.setAccessible(true);
+        String nameValue = (String) nameField.get(obj);
+        int hashBefore = nameValue.hashCode();
+        System.out.println("Before >>>> " + nameField.get(obj));
+
+        nameField.set(obj, ((String) nameField.get(obj)).concat(" Max"));
+        int hashAfter = nameValue.hashCode();
+        System.out.println("After >>>> " + nameField.get(obj));
+        System.out.println(hashBefore == hashAfter);
+
+    }
+}
