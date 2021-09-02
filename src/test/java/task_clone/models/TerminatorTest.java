8eafa64c118eec728ae204a1cef4224c8426afd9
Aleksandr Shabanov <vonabahs.a@gmail.com>
2021-09-02 18:21:26 +0300

    Add @Test deepCloneKryoTest

--- src/test/java/task_clone/models/TerminatorTest.java
+++ src/test/java/task_clone/models/TerminatorTest.java
@@ -1,5 +1,6 @@
 package task_clone.models;
 
+import com.rits.cloning.Cloner;
 import org.junit.jupiter.api.Test;
 
 import java.util.Calendar;
@@ -29,6 +30,27 @@ class TerminatorTest {
         assertFalse(expectedOriginal.getSkyNet().equals(actualClone.getSkyNet()));
         assertFalse(expectedOriginal.getSkyNet() == actualClone.getSkyNet());
 
+    }
+
+    @Test
+    public void deepCloneKryoTest() {
+        //Given
+        Calendar calendar = new GregorianCalendar(1997,Calendar.AUGUST,4);
+        Date birthdaySkyNet = calendar.getTime();
+        SkyNet skyNet = new SkyNet(birthdaySkyNet);
+
+        Terminator expectedOriginal = new Terminator("T800", 2029, skyNet);
+
+        //When
+        Cloner cloner = new Cloner();
+        Terminator actualClone = cloner.deepClone(expectedOriginal);
+
+        //Then
+        assertTrue(expectedOriginal.getModelName().equals(actualClone.getModelName()));
+        assertTrue(expectedOriginal.getModelName() == actualClone.getModelName());
+        assertTrue(expectedOriginal.getYearOfIssue() == actualClone.getYearOfIssue());
+        assertFalse(expectedOriginal.getSkyNet().equals(actualClone.getSkyNet()));
+        assertFalse(expectedOriginal.getSkyNet() == actualClone.getSkyNet());
     }
 
 }
